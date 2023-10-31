218f52aba1744a73630bdf41498f3846f13450a1
Suarez Agustina <suarezagustina12.12@gmail.com>
2023-10-31 14:56:27 -0300

    edad y avisa si puede conducir

---
+++ js/tp2.ejercicio1bucles.js
@@ -0,0 +1,15 @@
+let edad
+while (isNaN(edad)) {
+  try {
+    edad = parseInt(prompt("Edad:"))
+    if (isNaN(edad)) {
+      throw new Error("La edad ingresada no es un número válido.")
+    }
+  } catch (error) {
+    alert(error.message)
+  }
+}
+
+if (edad >= 18) {
+  alert("Puedes conducir")
+}
